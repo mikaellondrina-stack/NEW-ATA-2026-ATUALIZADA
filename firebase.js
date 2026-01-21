34adf0d921817969e3a31b62bb1550aac48c200f
mikaellondrina-stack <mikaellondrina@gmail.com>
2026-01-21 12:37:48 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,15 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAzfPWTcBtJk3UyOYdLIeSK3PlfjYKJAHI",
   authDomain: "site-universal-29a2b.firebaseapp.com",
-  databaseURL: "https://site-universal-29a2b-default-rtdb.firebaseio.com",
   projectId: "site-universal-29a2b",
   storageBucket: "site-universal-29a2b.firebasestorage.app",
   messagingSenderId: "793824632619",
   appId: "1:793824632619:web:e035c64e33969a40932f6e"
 };
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Cria a variável global do banco
+const db = firebase.firestore();
+
